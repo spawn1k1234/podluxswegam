10e75aed5a97fd7938833993a9e922a324aba34b
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-05-23 14:59:36 +0200

    Fix1

--- src/components/pages/showcasePages/ShowcasePage/ShowcasePage.tsx
+++ src/components/pages/showcasePages/ShowcasePage/ShowcasePage.tsx
@@ -1,17 +1,71 @@
+// import { Outlet } from "react-router-dom";
+// import ShowcaseFooter from "../../../layouts/showcaseLayouts/ShowcaseFooter/ShowcaseFooter";
+// import ShowcaseHeader from "../../../layouts/showcaseLayouts/ShowcaseHeader/ShowcaseHeader";
+// import ShowcaseMain from "../../../layouts/showcaseLayouts/ShowcaseMain/ShowcaseMain";
+// import classes from "./ShowcasePage.module.css";
+
+// const ShowcasePage: React.FC = () => {
+//   return (
+//     <div className={classes.showcase}>
+//       <ShowcaseHeader />
+//       <ShowcaseMain>
+//         <>
+//           <Outlet />
+//         </>
+//       </ShowcaseMain>
+//       <div>
+//         <ShowcaseFooter />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ShowcasePage;
+import { useEffect, useState, useRef } from "react";
 import { Outlet } from "react-router-dom";
 import ShowcaseFooter from "../../../layouts/showcaseLayouts/ShowcaseFooter/ShowcaseFooter";
 import ShowcaseHeader from "../../../layouts/showcaseLayouts/ShowcaseHeader/ShowcaseHeader";
 import ShowcaseMain from "../../../layouts/showcaseLayouts/ShowcaseMain/ShowcaseMain";
 import classes from "./ShowcasePage.module.css";
+import video from "../../../../assets/podluxsweglog.mp4";
 
 const ShowcasePage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
+  useEffect(() => {
+    const videoEl = videoRef.current;
+    if (videoEl) {
+      videoEl.play();
+      const onEnd = () => setIsLoading(false);
+      videoEl.addEventListener("ended", onEnd);
+      return () => videoEl.removeEventListener("ended", onEnd);
+    } else {
+      // fallback in case video can't be loaded
+      const timeout = setTimeout(() => setIsLoading(false), 3000);
+      return () => clearTimeout(timeout);
+    }
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className={classes.videoWrapper}>
+        <video
+          ref={videoRef}
+          className={classes.video}
+          src={video}
+          autoPlay
+          muted
+        />
+      </div>
+    );
+  }
+
   return (
     <div className={classes.showcase}>
       <ShowcaseHeader />
       <ShowcaseMain>
-        <>
-          <Outlet />
-        </>
+        <Outlet />
       </ShowcaseMain>
       <div>
         <ShowcaseFooter />
