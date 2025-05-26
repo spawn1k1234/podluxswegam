6729edbc2908f38c2dff0efc853b373dd4bdf61f
spawn1k1234 <vladislav.sidorov2008@gmail.com>
2025-05-26 18:24:33 +0200

    Fix1

--- src/components/pages/showcasePages/NotFound/NotFound.tsx
+++ src/components/pages/showcasePages/NotFound/NotFound.tsx
@@ -1,23 +1,23 @@
-import { Link } from 'react-router-dom';
-import { PATHS } from '../../../../constants/routes';
-import SectionBody from '../../../layouts/showcaseLayouts/Section/SectionBody/SectionBody';
-import Placeholder from '../../../UI/Placeholder/Placeholder';
-import classes from './NotFound.module.css';
+import { Link } from "react-router-dom";
+import { PATHS } from "../../../../constants/routes";
+import SectionBody from "../../../layouts/showcaseLayouts/Section/SectionBody/SectionBody";
+import Placeholder from "../../../UI/Placeholder/Placeholder";
+import classes from "./NotFound.module.css";
 
 const NotFound: React.FC = () => {
   return (
     <SectionBody>
-      <div className={classes['not-found']}>
-        <Placeholder text={'Такой страницы нет'} />
+      <div className={classes["not-found"]}>
+        <Placeholder text={"Такой страницы нет"} />
         <span className={classes.title}>404</span>
 
-        <div className={classes['wrapper']}>
+        <div className={classes["wrapper"]}>
           <span>Перейти</span>
-          <div className={classes['link-wrapper']}>
+          <div className={classes["link-wrapper"]}>
             <Link to={PATHS.showcase} className={classes.link}>
               На главную
             </Link>
-            <Link to={`${PATHS.admin}${PATHS.orders}`} className={classes.link}>
+            {/* <Link to={`${PATHS.admin}${PATHS.orders}`} className={classes.link}>
               В админку
             </Link>
 
@@ -28,7 +28,7 @@ const NotFound: React.FC = () => {
               rel="noreferrer"
             >
               В репозиторий
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
